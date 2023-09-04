d36b76d7c2f2c6592d71f18fd6c508fb1ab30d57
Dracsis Cella De Cavalli Almeida <dcella-d@c2r7s5.42lisboa.com>
2023-09-04 16:26:34 +0100

    good now

--- part1.cpp
+++ part1.cpp
@@ -2,54 +2,87 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <cstring>
+#include <cerrno>
+#include <fcntl.h>
+#include <ctime>
 #include <unistd.h>
 #include <arpa/inet.h>
+# include <sys/wait.h>
 
-void monitorPort(int port) {
+// this massive function will try to connect to the open port
+// and wait for some IP to connect. If in less than 10 seconds
+// someone connect to the port it will print the IP.
+
+int monitorPort(int port) {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
-        std::cerr << "Error creating socket." << std::endl;
-        return;
+        perror("Error creating socket");
+        return 1;
     }
 
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port); // Change to your desired port
 
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error binding to port." << std::endl;
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        perror("Error binding");
         close(serverSocket);
-        return;
+        return 1;
+    }
+
+    // Set the socket to non-blocking mode
+    int flags = fcntl(serverSocket, F_GETFL, 0);
+    if (flags == -1) {
+        perror("Error getting socket flags");
+        close(serverSocket);
+        return 1;
+    }
+    if (fcntl(serverSocket, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("Error setting socket to non-blocking");
+        close(serverSocket);
+        return 1;
     }
 
     if (listen(serverSocket, 5) == -1) {
-        std::cerr << "Error listening on port." << std::endl;
+        perror("Error listening");
         close(serverSocket);
-        return;
+        return 1;
     }
 
-    std::cout << "Waiting for connections on port " << port << "..." << std::endl;
+    // Set a timeout for listening (e.g., 10 seconds)
+    time_t start_time = time(NULL);
+    time_t timeout = 10; // Adjust this value as needed
 
     while (true) {
-        struct sockaddr_in clientAddr;
-        socklen_t clientAddrSize = sizeof(clientAddr);
-        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrSize);
-        if (clientSocket == -1) {
-            std::cerr << "Error accepting connection." << std::endl;
-            continue;
+        if (time(NULL) - start_time >= timeout) {
+            std::cout << "Timeout reached. No incoming connections." << std::endl;
+            break;
         }
 
-        char clientIP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+        int clientSocket = accept(serverSocket, NULL, NULL);
 
-        std::cout << "Connection from IP: " << clientIP << " on port " << port << std::endl;
-
-        close(clientSocket);
+        if (clientSocket == -1) {
+            // Check for EWOULDBLOCK to handle non-blocking socket
+            if (errno == EWOULDBLOCK) {
+                // No incoming connection yet, continue waiting
+                continue;
+            } else {
+                perror("Error accepting connection");
+                break;
+            }
+        } else {
+            std::cout << "Incoming connection accepted." << std::endl;
+            // Handle the incoming connection
+            close(clientSocket);
+        }
     }
 
     close(serverSocket);
+    return 0;
 }
+
+// this function is just for check if the port is open in a very simple way.
 
 bool isPortOpen(int port) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -73,20 +106,51 @@ bool isPortOpen(int port) {
     return true;
 }
 
-int main() {
-    int port = 0; // Change this to the port you want to monitor
-	int count = 0;
+// this function is to run the script.
 
+int runscript(int check)
+{
+    int status;
+    int pid = fork();
+
+    if (pid == 0)
+    {
+        if (check == 1)
+            system("bash script.sh");
+        if (check == 2)
+            system("bash script2.sh"); 
+    }
+    else if (pid > 0)
+        waitpid(pid, &status, 0);
+    // printf("%d", WIFEXITED(status));
+    return status; //WIFEXITED(status) && WEXITSTATUS(status);
+}
+
+// the main function will loop 1 to 1024 port to check if it is open and then run the script.
+
+int main() {
+    int port = 0;
+	int count = 0;
+    int status = 0;
+
+    std::cout << "CHECKING PORTS: " << std::endl;
 	while (++port <= 1024)
 	{
 		if (isPortOpen(port))
 		{
 			count++;
     		monitorPort(port);
+            std::cout << std::endl;
 		}
 	}
 	if (count < 1)
+    {
 		std::cout << "No ports open. Nice!" << std::endl;
-
+    }
+    std::cout << "CHECKING LOGIN: " << std::endl;
+    if (runscript(1))
+    {
+        runscript(2);
+    }
     return 0;
 }
